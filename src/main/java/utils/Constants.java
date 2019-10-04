4533bef3c06a316f0d5faaf8c60c6c7771779cad
Pavan Kumar S <pavankumar.cse88@gmail.com>
2019-10-04 15:01:17 +0530

    <Pavan> Removed api key

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -3,7 +3,7 @@ package utils;
 public class Constants {
 
     public static final String omDbBaseUrl = "http://www.omdbapi.com/";
-    public static final String apiKey = "2038f1fc";
+    public static final String apiKey = "";
 
     public static final String basePath = System.getProperty("user.dir");
     public static final String report = "CompareMovieDetailsReport.html";
